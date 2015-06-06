ddc32999d99e88dacdee58a5a3f748ebd38cc82b
rodneyxr <rodneyxr@gmail.com>
2015-06-06 14:20:37 -0500

    added LuckyLogger for debug logging

--- src/edu/utsa/fileflow/Main.java
+++ src/edu/utsa/fileflow/Main.java
@@ -4,19 +4,26 @@ import java.io.File;
 import java.io.FileNotFoundException;
 
 import edu.utsa.fileflow.compiler.Compiler;
+import edu.utsa.fileflow.utilities.LuckyLogger;
 
 public class Main {
+	
+	public static final boolean DEBUG = true;
+	public static final LuckyLogger logger = new LuckyLogger("DEBUG");
 
 	public static void main(String[] args) {
-		System.out.println("Hello, World! Welcome to File Flow Analysis!");
+		logger.setEnabled(DEBUG);
+		Main.logger.log("Hello, World! Welcome to File Flow Analysis!");
+		
+		// create a file for the script to compile
 		File file = new File("test.script");
-		Compiler compile;
+		Compiler compile = null;
 
 		try {
 			compile = new Compiler(file);
 		} catch (FileNotFoundException e) {
 			System.err.println(e.getMessage());
-			return;
+			System.exit(1);
 		}
 		
 		compile.compile();
