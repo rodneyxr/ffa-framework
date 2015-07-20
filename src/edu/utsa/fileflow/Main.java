db30720fd19612ae237a989ef3947cd307b0315a
steven <stevenpetroff@gmail.com>
2015-07-20 12:33:50 -0500

    here rodney

--- src/edu/utsa/fileflow/Main.java
+++ src/edu/utsa/fileflow/Main.java
@@ -19,33 +19,35 @@ public class Main {
 	
 	public static void main(String[] args) {
 		logger.setEnabled(DEBUG);
-		
-		System.out.println("File Flow Analysis Version "+version+"\nWritten by Rodney Rodriguez and Steven Petroff \nfor The University of Texas at San Antonio");
-		System.out.println("\nMenu:");
-		System.out.println(" 1. To select a file to load type \"load\" or \"1\"");
-		System.out.println(" 2. To use stdin to define a new script type \"stdin\" or \"2\"");
-		System.out.println(" 3. To review the command dictionary type \"help\" or \"3\"");
-		System.out.println("To quit the program type \"quit or q\"");
-		String cmd = input.nextLine();
-		
-		while(!cmd.toLowerCase().equals("q") && !cmd.toLowerCase().equals("quit")){
-			if(cmd.equals("1") || cmd.equals("load")){
-				System.out.println("Please specify the path of the file: ");
-				String filepath = input.nextLine();
-				loadFile(filepath);
-			}
+		if(!DEBUG){
+			System.out.println("File Flow Analysis Version "+version+"\nWritten by Rodney Rodriguez and Steven Petroff \nfor The University of Texas at San Antonio");
+			System.out.println("\nMenu:");
+			System.out.println(" 1. To select a file to load type \"load\" or \"1\"");
+			System.out.println(" 2. To use stdin to define a new script type \"stdin\" or \"2\"");
+			System.out.println(" 3. To review the command dictionary type \"help\" or \"3\"");
+			System.out.println("To quit the program type \"quit or q\"");
+			String cmd = input.nextLine();
 			
-			if(cmd.equals("2") || cmd.equals("stdin")){
-				option_stdin();
-			}
-			if(cmd.equals("3") || cmd.equals("help")){
-				printCommandDictionary();
+			while(!cmd.toLowerCase().equals("q") && !cmd.toLowerCase().equals("quit")){
+				if(cmd.equals("1") || cmd.equals("load")){
+					System.out.println("Please specify the path of the file: ");
+					String filepath = input.nextLine();
+					loadFile(filepath);
+				}
 				
+				if(cmd.equals("2") || cmd.equals("stdin")){
+					option_stdin();
+				}
+				if(cmd.equals("3") || cmd.equals("help")){
+					printCommandDictionary();
+					
+				}
+				
+				cmd = input.nextLine();
 			}
-			
-			cmd = input.nextLine();
+		} else {
+			loadFile("test.script");
 		}
-		
 	}
 	
 	private static void option_stdin() {
@@ -81,7 +83,6 @@ public class Main {
 			e.printStackTrace();
 		}
 		loadFile("tempfile.txt");
-				
 	}
 
 	private static void printCommandDictionary() {
@@ -96,7 +97,6 @@ public class Main {
 		// create a file for the script to compile
 		File file = new File(fp);
 		Compiler compiler = new Compiler();
-
 		FileStruct preCondition = null;
 		try {
 			preCondition = compiler.compile(file);
