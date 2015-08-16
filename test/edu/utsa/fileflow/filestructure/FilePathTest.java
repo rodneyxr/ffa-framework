e362f9276ae6dfaab07828fed663c8f57bcab011
Rodney Rodriguez <rodneyxr@gmail.com>
2015-08-16 17:14:38 -0500

    Setup testing to work with eclipse

--- test/edu/utsa/fileflow/filestructure/FilePathTest.java
+++ test/edu/utsa/fileflow/filestructure/FilePathTest.java
@@ -1,31 +1,44 @@
+/**
+ * 
+ */
 package edu.utsa.fileflow.filestructure;
 
+import static org.junit.Assert.*;
+
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import static org.junit.Assert.assertEquals;
-
 /**
- * Created by Rodney Rodriguez on 8/16/15.
+ * @author Rodney
  *
- * This test class is used for testing the functionality of the FileStruct class.
  */
 public class FilePathTest {
 
-    @Rule
-    public ExpectedException expex = ExpectedException.none();
+	@Rule
+	public ExpectedException expex = ExpectedException.none();
 
-    public final String[] VALID_FILE_PATHS = { "root", "root/", "file1", "./file1", "root/file1", "root/dir1/file1",
-        "root/dir1/dir2", "./root/dir1/dir2/file1" };
+	public final String[] VALID_FILE_PATHS = { "root", "root/", "file1", "./file1", "root/file1", "root/dir1/file1",
+			"root/dir1/dir2", "./root/dir1/dir2/file1" };
 
-    @Before
-    public void setUp() throws Exception {
-    }
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
 
-    @Test
-    public void testFilePathCreation() throws Exception {
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	@Test
+    public void testFilePathCreation() {
         FilePath filepath;
 
         // test paths as directories
@@ -41,7 +54,5 @@ public class FilePathTest {
         }
 
     }
-
-
 
 }
