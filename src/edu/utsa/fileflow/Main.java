84ef17ffac2aa6535f7bd199ad9ac3478ee8eed9
Rodney Rodriguez <rodneyxr@gmail.com>
2015-08-26 10:39:42 -0500

    Clean Compiler class

--- src/edu/utsa/fileflow/Main.java
+++ src/edu/utsa/fileflow/Main.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 import edu.utsa.fileflow.compiler.Compiler;
 import edu.utsa.fileflow.compiler.CompilerException;
-import edu.utsa.fileflow.compiler.Condition;
+import edu.utsa.fileflow.compiler.ConditionManager;
 import edu.utsa.fileflow.utilities.LuckyLogger;
 
 /**
@@ -24,28 +24,15 @@ public class Main {
 		// create a file for the script to compile
 		File file = new File("scripts/test.script");
 		Compiler compiler = new Compiler();
-		Condition preCondition = null;
+		ConditionManager conditionManager = null;
 		try {
-			preCondition = compiler.compile(file);
+			conditionManager = compiler.compile(file);
 		} catch (CompilerException ce) {
 			System.err.println(ce.getMessage());
 			System.exit(-1);
 		}
 
-		// print the precondition
-		System.out.println("-------------");
-		System.out.println("Precondition");
-		System.out.println("-------------");
-		System.out.println(preCondition);
-
-		System.out.println();
-
-		// print the postcondition
-		System.out.println("--------------");
-		System.out.println("Postcondition");
-		System.out.println("--------------");
-		System.out.println(compiler.getPostCondition());
-
+		conditionManager.print();
 	}
 
 }
--- src/edu/utsa/fileflow/compiler/Compiler.java
+++ src/edu/utsa/fileflow/compiler/Compiler.java
@@ -4,28 +4,22 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-import edu.utsa.fileflow.Main;
-import edu.utsa.fileflow.filestructure.FileFlowWarning;
-import edu.utsa.fileflow.filestructure.FilePath;
-import edu.utsa.fileflow.filestructure.FileStructureException;
-
 public class Compiler {
 
-	// precondition (what the structure must look like before)
-	private Condition pre;
-
-	// postcondition (what the structure will look like after)
-	private Condition post;
+	// contains and manages the precondition and postcondition
+	private ConditionManager cm;
 
 	/**
-	 * Parses the test script and will return a Directory Structure Object
+	 * Parses the build script and compiles the precondition and postcondition
+	 * of the script using a condition manager
 	 * 
 	 * @param file
-	 *            The file to compile the conditions from.
-	 * @return The precondition
+	 *            The file to compile the conditions from
+	 * @return a ConditionManager object containing the precondition and
+	 *         postcondition
 	 * @throws CompilerException
 	 */
-	public Condition compile(File file) throws CompilerException {
+	public ConditionManager compile(File file) throws CompilerException {
 		// open the file for reading
 		Scanner scanner = null;
 		try {
@@ -34,12 +28,10 @@ public class Compiler {
 			throw new CompilerException(fnfe.getMessage());
 		}
 
-		// instantiate the precondition file structure
-		pre = new Precondition();
+		// instantiate the condition manager which contains our precondition and
+		// post condition
+		cm = new ConditionManager();
 
-		// instantiate the postcondition (current structure while we execute commands)
-		post = new Postcondition();
-		
 		// while we have more commands to read
 		while (scanner.hasNext()) {
 			// parse line to command object
@@ -63,15 +55,16 @@ public class Compiler {
 
 			switch (cmd.getType()) {
 			case touch:
-//				handleNew(cmd);
+				// TODO: implement touch
 				break;
 			case mkdir:
+				// TODO: implement mkdir
 				break;
 			case cp:
-//				handleCopy(cmd);
+				// TODO: implement cp
 				break;
 			case rm:
-//				handleDelete(cmd);
+				// TODO: implement rm
 				break;
 			default:
 				scanner.close();
@@ -80,138 +73,7 @@ public class Compiler {
 		}
 
 		scanner.close();
-		return pre;
-	}
-
-	/**
-	 * Assumes that a file path exists in the precondition. In the special case that it does not exist in either the precondition or the postcondition the file path will be added
-	 * to both because we will assume that path exists before the script was executed.
-	 * 
-	 * @param filePath The file path we want to assume exists.
-	 * @return True if we CAN assume that filePath existed before we ran the script.
-	 * @throws FileStructureException 
-	 * @throws FileFlowWarning 
-	 */
-	private boolean assume(FilePath filePath) throws FileStructureException, FileFlowWarning {
-		boolean inPre = pre.existsInPositive(filePath);
-		boolean $inPre = !pre.existsInNegative(filePath); // negate because it can exist then it is in pre
-		boolean inPost = post.existsInPositive(filePath);
-//		boolean $inPost = !post.canExist(filePath);
-	
-//		if ($inPost) {
-//			Main.logger.log("%s cannot exist in post condition", filePath);
-//			return false;
-//		}
-		
-		if (inPre) {
-			// if path in precondition but not in the post then it was deleted/moved at some point
-			// because it was already used as a precondition and was added to the postcondition at
-			// the same time. So if it is no longer in the post condition then it does not exist.
-			if (!inPost) {
-				Main.logger.log("%s does not exist", filePath);
-				return false;// throw new CompilerException(String.format("%s does not exist", filePath));
-			} // if path exists in both then it is valid
-		} else {
-			// if path does not exist in the precondition but exists in post
-			// do nothing because is not a precondition but still a valid argument
-			// else if it does not exists in either then it becomes a precondition
-			if (!inPost) {
-				if ($inPre) {
-					Main.logger.log("%s cannot be assumed", filePath);
-					return false;
-				}
-				pre.insertPositive(filePath);
-				post.insertPositive(filePath);
-			}
-		}
-
-		return true;
-	}
-
-	private void handleCopy(Command cmd) throws CompilerException {
-//		// assert command is legal
-//		if (cmd.getSize() != 3) {
-//			throw new CompilerException(String.format("'%s': Command '%s' expects exactly 2 arguments", cmd, cmd.getType().getName()));
-//		}
-//
-//		// handle the first argument
-//		FilePath arg1 = new FilePath(cmd.getArg(1), true);
-//		if (!assume(arg1)) {
-//			throw new CompilerException(String.format("'%s': arg1 does not exist", cmd));
-//		}
-//
-//		// the second argument should not exist, so if it does throw an exception
-//		FilePath arg2 = new FilePath(cmd.getArg(2), true);
-//		// in order for the second argument to be valid the path to the file must exist but not the file itself
-//		if (!assume(arg2.getPathToFile())) {
-//			throw new CompilerException(String.format("'%s': path to arg2 does not exist", cmd));
-//		}
-//
-//		if (post.exists(arg2)) {
-//			throw new CompilerException(String.format("'%s': File or directory already exists in postcondition.\n%s", cmd, post));
-//		}
-//
-//		// insert clone of the path to the new path
-//		FileStruct efs = post.getFileStruct(true);
-//		efs.insert(efs.getFileStruct(arg1).clone(), arg2);
-	}
-
-	private void handleMove(Command cmd) throws CompilerException {
-//		handleCopy(cmd);
-//		FilePath arg1 = new FilePath(cmd.getArg(1), true);
-//
-//		// delete arg1 from file structure
-//		post.remove(arg1, true);
-//		post.insert(arg1, false);
-	}
-
-	private void handleDelete(Command cmd) throws CompilerException {
-//		// assert command is legal
-//		if (cmd.getSize() != 2) {
-//			throw new CompilerException(String.format("'%s': Command '%s' expects only 1 argument", cmd, cmd.getType().getName()));
-//		}
-//
-//		// first argument must exist so add it to precondition file structure if it isn't in post
-//		FilePath arg1 = new FilePath(cmd.getArg(1), true);
-//
-//		// if it exists in post then it doesn't need to be in precondition
-//		// because it was already created by another command
-//		if (!assume(arg1)) {
-//			throw new CompilerException(String.format("'%s': File does not exist", cmd));
-//		}
-//
-//		post.remove(arg1, true);
-//		post.insert(arg1, false);
-	}
-
-	private void handleNew(Command cmd) throws CompilerException {
-//		// assert command is legal
-//		if (cmd.getSize() != 2) {
-//			throw new CompilerException(String.format("'%s': Command '%s' expects only 1 argument", cmd, cmd.getType().getName()));
-//		}
-//
-//		// arg1 should not exist in either pre or post
-//		FilePath arg1 = new FilePath(cmd.getArg(1), true);
-//		
-//		if (post.exists(arg1)) {
-//			throw new CompilerException(String.format("'%s': File or directory already exists in postcondition.\n%s", cmd, post));
-//		}
-//		
-////		if (post.exists(arg1) && !assume(arg1)) {
-////			throw new CompilerException(String.format("'%s': File or directory already exists in postcondition.\n%s", cmd, post));
-////		}
-//
-//		// assert it doesn't exist in precondition
-//		if (pre.canExist(arg1) && pre.exists(arg1)) {
-//			pre.insert(arg1, false);
-//		}
-//
-//		post.insert(arg1, true);
-//		post.remove(arg1, false);
-	}
-
-	public Condition getPostCondition() {
-		return post;
+		return cm;
 	}
 
 }
